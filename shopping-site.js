e8eb9fe5978f13dfd9a928a13549862bc233c462
EC2 Default User <ec2-user@ip-172-31-33-248.ec2.internal>
2019-09-29 04:36:53 +0000

    add 2019/09/29

---
+++ shopping-site.js
@@ -0,0 +1,11 @@
+$(window).load(function(){
+  $(".product_lists_li img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#product_main_img img").attr("src", img_src);
+  });
+});
+$(document).ready(function(){
+  $("#sp_navi_btn").click(function(){
+    $(".sp_navi_li").slideToggle();
+  });
+});
